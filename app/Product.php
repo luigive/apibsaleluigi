dd6d10916f263dd5915399e0d518b52ace8712b2
luigive <luigivenegoni2808@gmail.com>
2021-05-06 21:49:13 -0400

    Creacion de Modelos.

---
+++ app/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name', 'ur_image', 'price','discount','category',
+    ];
+
+    //esto tiene el nombre de la tabla que voy a relacionar, todo con miniscula
+    //tambien el category es para asignar category como " llave foranea"
+    public function category(){
+        return $this->belongsTo(Category::class,'category');
+    }
+}
+
+
